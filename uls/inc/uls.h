1d1af55d83f1dc30eb1d4086018fee481e91cfca
Yaroslav Pratsovity <ypratsovit@e2r4p4.unit.ua>
2019-12-19 16:56:15 +0200

    test1

--- uls/inc/uls.h
+++ uls/inc/uls.h
@@ -1,5 +1,9 @@
 #ifndef ULS_H
 #define ULS_H
-#include "../libmx/inc/libmx.h"
 
+#include "../libmx/inc/libmx.h"
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 #endif
--- uls/src/main.c
+++ uls/src/main.c
@@ -1,7 +1,40 @@
 #include "../inc/uls.h"
 
+// static void test1_readdir(const char *argv[]) {
+//     DIR *dir;
+//     struct dirent *entry;
+
+//     dir = opendir(argv[1]);
+//     if (!dir) {
+//         perror("diropen");
+//         exit(1);
+//     };
+//     while ( (entry = readdir(dir)) != NULL) {
+//         mx_printstr(entry->d_name);
+//         mx_printstr("\t");
+//     }
+//     mx_printstr("\n");
+//     closedir(dir);
+// }
+
+// static void test1_stat() {
+//     char *date;
+//         int ret;
+//         struct stat buf;
+//         if ((ret = stat("test.txt", &buf))!=0)
+//           {
+//             fprintf(stderr, "stat failure error .%d", ret);
+//             abort();
+//           }
+//         date = asctime(localtime(&buf.st_ctime));
+//         printf("\n %s\n", date);
+//         printf("\n %d mode\n", buf.st_mode);
+//         printf("\n %lld size\n", buf.st_size);
+// }
+
 int main (int argc, char const *argv[]) {
-    argc++;
-    mx_printstr(argv[0]);
+    argc = 0;
+    argv = NULL;
+
     return 0;
 }
