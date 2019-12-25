9c0556be6dfe09f02f67518e69a4f664b8745b18
Dariia Mushynska <dmushynska@e2r4p2.unit.ua>
2019-12-25 17:05:19 +0200

    heh

--- uls/src/mx_errors.c
+++ uls/src/mx_errors.c
@@ -1,31 +1,43 @@
 #include "../inc/uls.h"
 
-static void check_fileorflag(int argc, char const *argv, int lol) {
-    argc--;
-    if (argv[0] == '-' && argv[1] == '-' && argv[2] == '\0') {
-        mx_printstr("YES");
-        return;
-    }
-    if (argv[0] == '-' && argv[1] == '-' && argv[2] != '\0') {
-        mx_printstr("usage: ./uls [-ab] [file ...]\n");
-        return;
-    }
-    if (argv[0] != '-') { // написать функцию, которая чекает не названия файла ли это
-        mx_printerr("usage: ./uls [-ab] [file ...]\n");
-        exit(0);
-    }
-    if (lol == 0) {
-        mx_printerr("usage: ./uls [-ab] [file ...]\n");
-        exit(0);
+static void check_fileordir(char const *argv[], int ind_char, int argc) {
+    while (ind_char < argc) {
+        if (mx_dirorfile(argv[ind_char]) == 1 
+            || (mx_dirorfile(argv[ind_char]) == 0)) {
+            mx_printstr("YES\n");
+        }
+        else {
+            mx_printerr("./uls ");
+            mx_printerr(argv[ind_char]);
+            mx_printerr(": No such file or directory");
+        }
+        ind_char++;
     }
 }
+
+// static void check_flag(char const *argv[], int lol, int ind_char) {
+//     // mx_printstr(" LOL ");
+//     // mx_printint(lol);
+//     if (argv[ind_char][0] == '-' && argv[ind_char][1] == '-' 
+//         && argv[ind_char][2] != '\0') {
+//         mx_printstr("usage: ./uls [-ab] [file ...]\n");
+//         return;
+//     }
+//     if (argv[ind_char][0] != '-') {
+//         mx_printerr("usage: ./uls [-ab] [file ...]\n");
+//         exit(0);
+//     }
+//     if (lol == 0) {
+//         mx_printerr("usage: ./uls [-ab] [file ...]\n");
+//         exit(0);
+//     }
+// }
 
 static void noassces(int argc, char const *argv[]) {
     char *flag = mx_strdup("ab");
     int lol = 0;
 
-argc++;
-    for (int ind_char = 1;  argv[ind_char] != NULL; ind_char++) {
+    for (int ind_char = 1;  ind_char < argc; ind_char++) {
         for (int index = 1; index < mx_strlen(argv[ind_char]); index++)
             for (int check = 0; check < 2; check++) {
                 if (argv[ind_char][index] == flag[check]) {
@@ -34,7 +46,12 @@ argc++;
                 }
                 lol = 0;
             }
-        check_fileorflag(argc, argv[ind_char], lol);
+        // check_flag(argv, lol, ind_char);
+        if (argv[ind_char][0] == '-' && argv[ind_char][1] == '-'
+            && argv[ind_char][2] == '\0') {
+            check_fileordir(argv, ++ind_char, argc);
+            return;
+        }
     }
     free(flag);
 }
