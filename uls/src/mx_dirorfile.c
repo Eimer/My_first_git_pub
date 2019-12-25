f9937bb65ba5c587fca1fbedd20f76d25cdaea46
Yaroslav Pratsovity <ypratsovit@e2r4p4.unit.ua>
2019-12-25 14:57:22 +0200

    mx_dirorfile

---
+++ uls/src/mx_dirorfile.c
@@ -0,0 +1,20 @@
+#include "../inc/uls.h"
+
+int mx_dirorfile(const char *obj) {
+    DIR *dir;
+    int file;
+
+    dir = opendir(obj);
+    if (dir) {
+        closedir(dir);
+        return 0;
+    }
+    else {
+        file = open(obj, O_RDONLY);
+        if(file != -1) {
+            close(file);
+            return 1;
+        }
+    }
+    return -1;
+}
