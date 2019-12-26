a52d92ec3283e88c8a69c23e37cdbd6483a0ce0f
Yaroslav Pratsovity <ypratsovit@e2r4p4.unit.ua>
2019-12-26 13:39:34 +0200

    mx_count_longest

--- uls/src/mx_count_longest.c
+++ uls/src/mx_count_longest.c
@@ -1,10 +1,47 @@
 #include "../inc/uls.h"
 
-int mx_count_longest(int argc, char const *argv[]) {
+static int logestfile(int argc, char const *argv[]) {
     int longest = 0;
-    // char **files = mx_arr_files(argc, argv);
-    // char **dirs = mx_arr_dirs(argc, argv);
-    argc = 0;
-    argv = NULL;
+    int count_files = 0;
+    char **files = mx_arr_files(argc, argv);
+    if (files) {
+        while (files[count_files]) {
+            if(mx_strlen(files[count_files]) > longest) {
+                longest = mx_strlen(files[count_files]);
+            }
+            count_files++;
+        }
+    }
     return longest;
+}
+
+static int longestdir(int argc, char const *argv[]) {
+    int longest = 0;
+    char **dirs = mx_arr_dirs(argc, argv);
+    DIR *dir;
+    struct dirent *entry;
+    int count = 0;
+    if (dirs) {
+        while (dirs[count] != NULL) {
+            dir = opendir(dirs[count]);
+            while ((entry = readdir(dir)) != NULL) {
+                if(mx_strlen(entry->d_name) > longest)
+                    longest =  mx_strlen(entry->d_name);
+            }
+            count++;
+        }
+    }
+    return longest;
+}
+
+int mx_count_longest(int argc, char const *argv[]) {
+    int longest_file = logestfile(argc, argv);
+    int longest_dir =  longestdir(argc, argv);
+    if (longest_file > longest_dir) {
+        return longest_file;
+    }
+    else {
+        return longest_dir; 
+    }
+    return 0;
 }
