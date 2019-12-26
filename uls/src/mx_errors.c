baa49169f203aa6e2105227f1dcaaee476566e81
Dariia Mushynska <dmushynska@e2r4p2.unit.ua>
2019-12-26 18:55:05 +0200

    Lenochka

--- uls/src/mx_errors.c
+++ uls/src/mx_errors.c
@@ -9,7 +9,7 @@ static void check_fileordir(char const *argv[], int ind_char, int argc) {
         if (mx_dirorfile(argv[ind_char]) == -1) {
             mx_printerr("./uls ");
             mx_printerr(argv[ind_char]);
-            mx_printerr(": No such file or directory");
+            mx_printerr(": No such file or directory\n");
         }
         ind_char++;
     }
@@ -17,36 +17,32 @@ static void check_fileordir(char const *argv[], int ind_char, int argc) {
 
 static int check_flag(int argc, char const *argv[], int lol, int ind_char) {
     if (argv[ind_char][0] == '-' && argv[ind_char][1] == '-'
-            && argv[ind_char][2] == '\0') {
+        && argv[ind_char][2] == '\0') {
             check_fileordir(argv, ++ind_char, argc);
+            return 1;
+    }
+    if (argv[ind_char][0] != '-') {
+        check_fileordir(argv, ind_char, argc);
+        // mx_printstr(" HEH ");
             return 1;
     }
     if (argv[ind_char][0] == '-' && argv[ind_char][1] == '-'
         && argv[ind_char][2] != '\0') {
         mx_printstr("usage: ./uls [-ab] [file ...]\n");
         exit(0);
-        // return 1;
-    }
-    if (argv[ind_char][0] != '-') {
-        check_fileordir(argv, ind_char, argc);
-            return 1;
     }
     if (lol == 0) {
         mx_printerr("usage: ./uls [-ab] [file ...]\n");
         exit(0);
-        // return 1;
     }
     return 0;
 }
 
-static void noassces(int argc, char const *argv[]) {
+void mx_errors(int argc, char const *argv[]) {
     char *flag = mx_strdup("ab");
     int lol = 0;
 
-    
     for (int ind_char = 1;  ind_char < argc; ind_char++) {
-        if (argc == 2)
-            check_flag(argc, argv, lol, ind_char);
         for (int index = 1; index < mx_strlen(argv[ind_char]); index++)
             for (int check = 0; check < 2; check++) {
                 lol = 0;
@@ -62,6 +58,3 @@ static void noassces(int argc, char const *argv[]) {
     free(flag);
 }
 
-void mx_errors(int argc, char const *argv[]) {
-    noassces(argc, argv);
-}
