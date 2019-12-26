3d5a2178456359fc75484ee753554709e601ef4c
Yaroslav Pratsovity <ypratsovit@e2r4p4.unit.ua>
2019-12-26 11:43:49 +0200

    some_fix

--- uls/src/mx_arr_files.c
+++ uls/src/mx_arr_files.c
@@ -10,7 +10,8 @@ char **mx_arr_files(int argc, char const *argv[]) {
         }
     }
     if (count_files != 0) {
-        arr_files = (char **)malloc(sizeof(char *) * count_files);
+        arr_files = (char **)malloc(sizeof(char *) * (count_files + 1));
+        arr_files[count_files] = NULL;
         count_files = 0;
         for (int i = 1; i < argc; i++) {
             if(mx_dirorfile(argv[i]) == 1) {
