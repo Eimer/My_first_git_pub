b7d84e6c9b2f45711ae502e85f90b88b6e8bed3d
Dariia Mushynska <dmushynska@e2r4p2.unit.ua>
2019-12-26 15:15:23 +0200

    lost

--- uls/src/mx_errors.c
+++ uls/src/mx_errors.c
@@ -1,18 +1,12 @@
 #include "../inc/uls.h"
 
-// static int check_next(int argc, int ind_char, char const *argv[]) {
-//     for (; ind_char < argc; ind_char++) {
-
-//     }
-// }
-
 static void check_fileordir(char const *argv[], int ind_char, int argc) {
     while (ind_char < argc) {
         if (mx_dirorfile(argv[ind_char]) == 1 
             || (mx_dirorfile(argv[ind_char]) == 0)) {
             mx_printstr("YES\n");
         }
-        else {
+        if (mx_dirorfile(argv[ind_char]) == -1) {
             mx_printerr("./uls ");
             mx_printerr(argv[ind_char]);
             mx_printerr(": No such file or directory");
@@ -22,23 +16,23 @@ static void check_fileordir(char const *argv[], int ind_char, int argc) {
 }
 
 static int check_flag(int argc, char const *argv[], int lol, int ind_char) { // 1
-    if (argv[ind_char][0] == '-' && argv[ind_char][1] == '-'
-            && argv[ind_char][2] == '\0') {
-            check_fileordir(argv, ++ind_char, argc);
+lol++;
+    // if (argv[ind_char][0] == '-' && argv[ind_char][1] == '-'
+    //         && argv[ind_char][2] == '\0') {
+    //         check_fileordir(argv, ++ind_char, argc);
+    //         return 1;
+    // }
+    // if (argv[ind_char][0] == '-' && argv[ind_char][1] == '-' 
+    //     && argv[ind_char][2] != '\0') {
+    //     mx_printstr("usage: ./uls [-ab] [file ...]\n");
+    //     return 1;
+    // }
+    if (argv[ind_char][0] != '-') {
+        check_fileordir(argv, ++ind_char, argc);
             return 1;
     }
-    if (argv[ind_char][0] == '-' && argv[ind_char][1] == '-' 
-        && argv[ind_char][2] != '\0') {
-        mx_printstr("usage: ./uls [-ab] [file ...]\n");
-        return 1;
-    }
-    if (argv[ind_char][0] != '-') {
-        // check_next();
-        // mx_printerr("usage: ./uls [-ab] [file ...]\n");
-        // exit(0);
-    }
-    if (lol == 0)
-        mx_printerr("usage: ./uls [-ab] [file ...]\n");
+    // if (lol == 0)
+    //     mx_printerr("usage: ./uls [-ab] [file ...]\n");
     return 0;
 }
 
@@ -50,6 +44,7 @@ static void noassces(int argc, char const *argv[]) { // 2
         for (int index = 1; index < mx_strlen(argv[ind_char]); index++)
             for (int check = 0; check < 2; check++) {
                 lol = 0;
+                check_flag(argc, argv, lol, ind_char);
                 if (argv[ind_char][index] == flag[check]) {
                     lol = 1;
                     break;
