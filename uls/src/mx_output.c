9790c4d2da9d6334f033579296f62c852089885b
Yaroslav Pratsovity <ypratsovit@e2r4p4.unit.ua>
2020-01-22 15:20:13 +0200

    test

--- uls/src/mx_output.c
+++ uls/src/mx_output.c
@@ -4,8 +4,7 @@ static int which_element(int argc, char const *argv[], char **sorted_overall_arr
     int col = mx_count_col(argc, argv);
     int amount_obj = mx_count_obj(sorted_overall_arr);
     int res = 0;
-    printf("%d ", amount_obj);
-    exit(0);
+
     if (amount_obj % col != 0) {
         res = amount_obj / col + 1;
     }
@@ -56,4 +55,3 @@ void mx_output(int argc, char const *argv[], char **overall_arr) {
     }
     mx_printchar('\n');
 }
-
