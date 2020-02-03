872629bb840c190186f332c2e7b230453d9f55d6
Dariia Mushynska <dmushynska@e2r4p2.unit.ua>
2020-02-03 11:00:21 +0200

    pull1

--- uls/src/main.c
+++ uls/src/main.c
@@ -1,17 +1,5 @@
 #include "../inc/uls.h"
 
-//       printf("\n %lld size\n", buf.st_size);
-// }
-
-// static void test2_getpwuid() {
-//    struct stat buf;
-//    lstat("test.txt", &buf);
-//    printf("\n %d uid\n", buf.st_uid);
-//    struct passwd *pwuid = getpwuid(buf.st_uid);
-//    printf("%s", pwuid->pw_name);
-//    write(1, &buf.st_size,10);
-// }
-
 int main (int argc, char **argv) {
     // t_add_in_func *audit = (t_add_in_func *)malloc(sizeof(t_add_in_func));
     // mx_uls_noatr(argc, argv);
