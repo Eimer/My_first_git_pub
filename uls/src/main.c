237708ce3bdf19d9c94043462ca67acd2cd8c674
Dariia Mushynska <dmushynska@e2r4p2.unit.ua>
2020-02-10 15:30:58 +0200

    lost

--- uls/src/main.c
+++ uls/src/main.c
@@ -8,26 +8,26 @@
 // }
 
 int main (int argc, char **argv) {
-    mx_quick(argv, 1, argc);
-    for (int u = 1; u < argc; u++) {
-        mx_printstr(argv[u]);
-        mx_printstr("\n");
-    }
-    // t_add_in_func *audit = (t_add_in_func *)malloc(sizeof(t_add_in_func));
-    // audit->flags = (int *)malloc(sizeof(int) * 10);
+    // mx_quick(argv, 1, argc);
+    // for (int u = 1; u < argc; u++) {
+    //     mx_printstr(argv[u]);
+    //     mx_printstr("\n");
+    // }
+    t_add_in_func *audit = (t_add_in_func *)malloc(sizeof(t_add_in_func));
+    audit->flags = (int *)malloc(sizeof(int) * 10);
 
-    // audit->main_return = 1;
-    // audit->argc = argc;
-    // audit->check = 0;
-    // audit->check_n = 0;
-    // for (int u = 0; u < 10; u++)
-    //     audit->flags[u] = 0;
-    // mx_errors(argc, argv, audit, "aARrl1tSdf");
-    // if (audit->check_n > 1)
-    //     audit->flags[0] = 1;
-    // mx_enter_fileordir(argc, argv, audit);
-    // free(audit->flags);
-    // free(audit);
-    // // system("leaks -q uls");
-    // return audit->main_return;
+    audit->main_return = 1;
+    audit->argc = argc;
+    audit->check = 0;
+    audit->check_n = 0;
+    for (int u = 0; u < 10; u++)
+        audit->flags[u] = 0;
+    mx_errors(argc, argv, audit, "aARrl1tSdf");
+    if (audit->check_n > 1)
+        audit->flags[0] = 1;
+    mx_enter_fileordir(argc, argv, audit);
+    free(audit->flags);
+    free(audit);
+    // system("leaks -q uls");
+    return audit->main_return;
 }
