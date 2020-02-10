9a32d1891f0c5c3b84865d06308e04be840ae733
Yaroslav Pratsovity <ypratsovit@e2r4p4.unit.ua>
2020-02-10 18:03:33 +0200

    test

--- uls/src/mx_dirorfile.c
+++ uls/src/mx_dirorfile.c
@@ -13,5 +13,9 @@ int mx_dirorfile(const char *obj) {
         return 1;
     if (S_ISLNK(obj_stat.st_mode))
         return 1;
+    if (S_ISBLK(obj_stat.st_mode))
+        return 1;
+    if (S_ISCHR(obj_stat.st_mode))
+        return 1;
     return -1;
 }
