57d3054204df6cd72183f59b37b0f9d4bac2ef31
Yaroslav Pratsovity <ypratsovit@e2r4p4.unit.ua>
2020-02-17 17:37:44 +0200

    fix_auditor

--- uls/src/mx_spaces_l.c
+++ uls/src/mx_spaces_l.c
@@ -25,20 +25,17 @@ int mx_longest_numbers_links(char *obj, t_add_in_func *audit) {
                 buff = mx_strjoin(obj, entry->d_name);
                 lstat(buff, &buf);
                 free(buff);
-                if(mx_count_numbers(buf.st_nlink) > longest)
+                if (mx_count_numbers(buf.st_nlink) > longest)
                     longest = mx_count_numbers(buf.st_nlink);
             }
-            else if (audit->flags[2] == 1){
+            else if (audit->flags[2] == 1) {
                 if (mx_strcmp(entry->d_name, ".") != 0
-                    && mx_strcmp(entry->d_name, "..") != 0) {
+                    && mx_strcmp(entry->d_name, "..") != 0)
                     longest = mx_longest_space (buff, entry, obj, longest);
-                }
             }
-            else {
-                if (entry->d_name[0] != '.') {
+            else
+                if (entry->d_name[0] != '.')
                     longest = mx_longest_space (buff, entry, obj, longest);
-                }
-            }
         }
         closedir(dir);
     }
@@ -49,25 +46,12 @@ int mx_longest_numbers_pwuid(char *obj, t_add_in_func *audit) {
     DIR *dir;
     struct dirent *entry;
     int longest = 0;
-    struct stat buf;
-    struct passwd *pwuid;
 
     audit->buff = NULL;
     if (mx_dirorfile(obj) == 0) {
         dir = opendir(obj);
         while ((entry = readdir(dir)) != NULL) {
-            audit->buff = mx_strjoin(obj, entry->d_name);
-            lstat(audit->buff, &buf);
-            free(audit->buff);
-            pwuid = getpwuid(buf.st_uid);
-            if (audit->flags[1] != 1) {
-                if (entry->d_name[0] != '.')
-                    if(mx_strlen(pwuid->pw_name) > longest)
-                        longest = mx_strlen(pwuid->pw_name);
-            }
-            else
-                if(mx_strlen(pwuid->pw_name) > longest)
-                    longest = mx_strlen(pwuid->pw_name);
+            longest = mx_numbers_pwuid (obj, entry, longest, audit);
         }
         closedir(dir);
     }
@@ -85,27 +69,12 @@ int mx_longest_numbers_pwgid(char *obj, t_add_in_func *audit) {
     if (mx_dirorfile(obj) == 0) {
         dir = opendir(obj);
         while ((entry = readdir(dir)) != NULL) {
-            audit->buff = mx_strjoin(obj, entry->d_name);
-            lstat(audit->buff, &buf);
-            free(audit->buff);
             groups = getgrgid(buf.st_gid);
             if (audit->flags[1] != 1) {
-                if (entry->d_name[0] != '.') {
-                    if (!groups) {
-                        if (mx_count_numbers(buf.st_gid) > longest)
-                            longest = mx_count_numbers(buf.st_gid);
-                    }
-                    else if (mx_strlen(groups->gr_name) > longest)
-                        longest = mx_strlen(groups->gr_name);
-                }
+                longest = mx_numbers_pwgid (obj, entry, longest, audit);
             }
             else {
-                if (!groups) {
-                    if (mx_count_numbers(buf.st_gid) > longest)
-                        longest = mx_count_numbers(buf.st_gid);
-                }
-                else if (mx_strlen(groups->gr_name) > longest)
-                    longest = mx_strlen(groups->gr_name);           
+                longest = mx_numbers_pwgid_without_a (obj, entry, longest, audit);           
             }
         }
         closedir(dir);
